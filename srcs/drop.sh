2f68ab23b9c76057504658e33d6b476e339d00b0
Gothmane <76252610+Guennach@users.noreply.github.com>
2024-05-19 15:07:59 +0000

    checking

--- srcs/drop.sh
+++ srcs/drop.sh
@@ -1,8 +1,9 @@
-# docker stop $(docker ps -a -q) || true
-# docker rm $(docker ps -a -q) || true
-# docker image rm -f $(docker image ls -q) || true
-# docker volume rm -f $(docker volume ls -q) || true
-# sudo rm -rf wordpress_data mariadb_data
-# mkdir -p wordpress_data
-# mkdir -p mariadb_data
-docker compose up --build
+docker stop $(docker ps -a -q) || true
+docker rm $(docker ps -a -q) || true
+docker image rm -f $(docker image ls -q) || true
+docker volume rm -f $(docker volume ls -q) || true
+sudo rm -rf wordpress_data mariadb_data
+mkdir -p wordpress_data
+mkdir -p mariadb_data
+docker compose build 
+docker compose  up -d
--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -7,8 +7,9 @@
 # done
 
 mkdir -p /var/www/html/wordpress
-chmod -R 755 /var/www/html/wordpress
+chmod -R 777 /var/www/html/wordpress
 chown -R www-data:www-data /var/www/html/wordpress
+# chown -R www-data:www-data /var/www/html/wordpress/*
 
 cd /var/www/html/wordpress
 
@@ -16,19 +17,21 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 
 chmod +x wp-cli.phar
 
-./wp-cli.phar core download --allow-root
+mv wp-cli.phar /usr/local/bin/wp
+
+# if [ -f ""]
+wp core download --allow-root
 
 echo "config file start"
 
-./wp-cli.phar config create --dbname=wordpress --dbuser=gothmane --dbpass=gothmane --dbhost=mariadb:3306 --allow-root
+wp config create --dbname=wordpress --dbuser=gothmane --dbpass=gothmane --dbhost=mariadb:3306 --allow-root
 
 echo "config file created"
 
-./wp-cli.phar core install --url=localhost --title=inception --admin_user=root --admin_password=root --admin_email=root@root.com --allow-root
+wp core install --url=localhost --title=inception --admin_user=root --admin_password=root --admin_email=root@root.com --allow-root
 
 echo "install done"
 
-# sed -i "s|listen = 127.0.0.1:9000|listen = 0.0.0.0:9000|g" /etc/php8.1/php-fpm.d/www.conf
+sed -i "s|listen = /run/php/php8.2-fpm.sock|listen = 0.0.0.0:9000|g" /etc/php/8.2/fpm/pool.d/www.conf
 
-# exec "$@"
-# exec php-fpm7.4 -F
+exec php-fpm8.2 -F
